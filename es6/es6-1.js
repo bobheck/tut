e334079d86df992268d7250a5c339b3382f0b8d5
Bob <bobheck11@gmail.com>
2018-01-13 16:00:01 -0600

    interim

---
+++ es6/es6-1.js
@@ -0,0 +1,19 @@
+
+// map
+
+var numbers = [1,2,3];
+
+var doubledFor = [];
+
+// doublednumbers using for loop
+for (var i = 0; i < numbers.length; i++) {
+    doubledFor.push(numbers[i] * 2);
+}
+
+// doublednumbers using Map
+var doubledMap = numbers.map(function(number) {
+    return number * 2;
+})
+
+console.log(doubledMap);
+console.log(doubledFor);
