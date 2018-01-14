693d1675cdb927cd17bca5ac6f0776612c19c224
Bob <bobheck11@gmail.com>
2018-01-13 22:00:19 -0600

    working on filter

---
+++ es6/es6-filter.js
@@ -0,0 +1,56 @@
+// filter helper
+
+var products = [
+    { name: 'cucumber', type: 'vegetable', qty: 0, price: 1 },
+    { name: 'banana', type: 'fruit', qty: 10, price: 15 },
+    { name: 'celery', type: 'vegetable', qty: 30, price: 13 },
+    { name: 'orange', type: 'fruit', qty: 3, price: 5 }
+];
+
+// filter by type
+
+// classic for loop
+var filteredProductsFor = [];
+
+for (var i =0; i < products.length; i++) {
+    if(products[i].type === 'fruit') {
+        filteredProductsFor.push(products[i]);
+    }
+}
+
+console.log(filteredProductsFor);
+
+// using filter helper
+var filteredProductsFilter = products.filter(function(product) {
+    return product.type === 'fruit';
+});
+
+console.log(filteredProductsFilter);
+
+// using passed variable
+var findProd = function(typ)
+{
+   return products.filter(function (product) {
+        return product.type === typ;
+    });
+}
+
+var filteredProductsVar = findProd('fruit');
+console.log(filteredProductsVar);
+var filteredProductsVar = findProd('vegetable');
+console.log(filteredProductsVar);
+var filteredProductsVar = findProd('meat');
+console.log(filteredProductsVar);
+
+
+
+// more about filter
+// type is veg, qty is greater than 0, price is less than 10
+
+var multipleFilter = products.filter(function(product) {
+    return product.type === 'vegetable'
+    && product.qty > 0
+    && product.price < 20
+});
+
+console.log(multipleFilter);
--- es6/es6-map.js
+++ es6/es6-map.js
@@ -70,3 +70,17 @@ var speeds = trips.map(function(trip) {
 });
 
 console.log(speeds);
+
+// assignment 3
+// Really Hard - Implementing 'Pluck'
+// This is a hard one!
+// Implement a 'pluck' function.  Pluck should accept an
+// array and a string representing a property name and
+// return an  array containing that property from each object.
+
+// solution
+function pluck(array, property) {
+    return array.map(function (arritem) {
+        return arritem[property];
+    });
+}
