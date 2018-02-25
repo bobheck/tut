dd760a71905b95fe9d577d8fcb674343ff2e860e
Bob <bobheck11@gmail.com>
2018-02-24 20:01:57 -0600

    refreshed datingapp.spa

--- aspnet-core-angular/DatingApp/DatingApp.SPA/e2e/app.e2e-spec.ts
+++ aspnet-core-angular/DatingApp/DatingApp.SPA/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { DatingApp.SPAPage } from './app.po';
 
 describe('dating-app.spa App', () => {
-  let page: AppPage;
+  let page: DatingApp.SPAPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new DatingApp.SPAPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
