38841d5b2c612be3fbded9ac9bd70a140c13af7e
Bob <bobheck11@gmail.com>
2018-03-31 16:32:29 -0500

    Working on EF course

---
+++ mosh_ef/CodeFirstDemo/CodeFirstDemo/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    public class Post
+    {
+        public long Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    
+}
---
+++ mosh_ef/moshEF1/moshEF1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moshEF1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MoshEF_DBFirstDemoEntities1();
+            var post = new Post()
+            {
+                Body = "Body1",
+                DatePublished = DateTime.Now,
+                Title = "Title1",
+            };
+
+            var post2 = new Post()
+            {
+                Body = "Body4",
+                DatePublished = DateTime.Now,
+                Title = "Title4",
+            };
+
+            context.Posts.Add(post);
+            context.Posts.Add(post2);
+
+
+            context.SaveChanges();
+
+        }
+    }
+}
