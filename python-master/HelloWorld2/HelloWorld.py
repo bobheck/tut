6079346dbe3b4b4fca1a0529f6db1f4f23f53dd3
Bob Heck <rmheck@sabreindustries.com>
2018-04-04 13:45:39 -0500

    learning python

--- python-master/HelloWorld2/HelloWorld.py
+++ python-master/HelloWorld2/HelloWorld.py
@@ -1,7 +1,27 @@
-print('Hello World 2 !')
-print(8**76)
-print()
-print("Python's strings are easy to use")
-print('We can even include "quotes" in strings')
-print("The End")
+# greeting = "Hello"
+# name = input("Enter name: ")
+# print(greeting + ' ' + name)
+#comment
 
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, he\'s resting')
+
+splitString2 = """This string has been
+split over
+several lines"""
+
+print(splitString2)
+
+
+tripleQuotes = '''Use triple quotes so quote's in the string are "ignored" '''
+print(tripleQuotes)
+
+
+
+print("""
+The End""")
