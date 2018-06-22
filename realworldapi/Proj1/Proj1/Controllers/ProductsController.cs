1685cee78c5c838fe288198e46b1610eaa4a9578
Bob Heck <rmheck@sabreindustries.com>
2018-06-22 15:56:50 -0500

    added realworldapi

---
+++ realworldapi/Proj1/Proj1/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+// https://docs.microsoft.com/en-us/aspnet/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2#route-constraints
+
+namespace Proj1.Controllers
+{
+    [RoutePrefix("api/products")]
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        [HttpGet, Route("")]
+        public IEnumerable<string> GetAllProducts()
+        {
+            return new string[] { "product1", "product2" };
+        }
+
+        // GET: api/Products/5
+        [HttpGet, Route("{id:int:range(1000,3000)}")]
+        public string Get(int id)
+        {
+            return "product";
+        }
+
+
+        // GET: api/Products/5/orders/custid
+        [HttpGet, Route("{id:int:range(1000,3000)}/orders/{custid}")]
+        public string Get(int id, string custid)
+        {
+            return $"product {id} -orders " + custid;
+        }
+
+        
+        // POST: api/Products
+        [HttpPost, Route("")]
+        public void CreateProduct([FromBody]string value)
+        {
+        }
+        // PUT: api/Products/5
+        [HttpPut, Route("{id:int:range(1000,3000)}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete, Route("{id:int:range(1000,3000)}")]
+        public void Delete(int id)
+        {
+        }
+        
+    }
+}
