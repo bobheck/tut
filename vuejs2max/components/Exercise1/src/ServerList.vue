2eba84287bfdfe114157f766d94488da941a8999
Bob Heck <rmheck@sabreindustries.com>
2018-09-21 16:26:13 -0500

    interim

---
+++ vuejs2max/components/Exercise1/src/ServerList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    v-for="index in 5">
+                Server #{{ index }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ServerList"
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid blue;
+    }
+</style>
---
+++ vuejs2max/components/comp1/src/App.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <app-server-status></app-server-status>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
