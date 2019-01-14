3ce06645ae2a5cef49b148ce3775d7a02f936dfd
Bob Heck <rmheck@sabreindustries.com>
2019-01-14 16:59:15 -0600

    eod, vuex1

---
+++ vuejs2max/components/comp-advanced/src/components/Author.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h3>The Author</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Author"
+    }
+</script>
+
+<style scoped>
+
+</style>
