c6e9e72036df232c28fefb8d319fcb671c1f511b
Bob Heck <rmheck@sabreindustries.com>
2019-02-22 16:27:53 -0600

    eod

---
+++ vuejs2max/vuex1/src/components/AnotherResult.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    <p>Counter is: {{ dcnt }}</p>
+    <p>Number of clicks: {{stringCounter}}</p>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    export default {
+        computed: mapGetters({
+            dcnt: 'doubleCounter'},
+            ['stringCounter']
+        ),
+
+        //     counter() {
+        //         return this.$store.getters.doubleCounter;
+        //     },
+        //     clicks() {
+        //         return this.$store.getters.stringCounter;
+        //     }
+        // }
+
+    }
+
+</script>
