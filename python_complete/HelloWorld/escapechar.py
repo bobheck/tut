b8ed72beee9dfb666e39906843d5b3f5c673b6f2
bobheck <bobheck11@gmail.com>
2020-03-23 17:10:50 -0500

    learning python

---
+++ python_complete/HelloWorld/escapechar.py
@@ -0,0 +1,26 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+tabbedString = "1\t2\t3\t4\t5 tabs"
+
+print(splitString)
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
+
+anotherSplitString = """This string has been
+split over
+several
+lines"""
+print(anotherSplitString)
+
+escapedSplitString = """This string has been \
+split but \
+ignored over \
+several \
+lines"""
+
+print(escapedSplitString)
+
+print("C:\\Users\\bhec\\notes.txt")
+print(r"C:\Users\bhec\notes.txt")
---
+++ python_complete/HelloWorld/operators.py
@@ -0,0 +1,14 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)  #integer division
+print(a % b)   #remainder after integer division
+
+print()
+
+for i in range(1, 4):
+    print(i)
