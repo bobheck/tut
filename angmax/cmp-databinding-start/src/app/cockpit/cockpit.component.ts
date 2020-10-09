35f2e2552956b50eefa5c64a3960746213d4ad80
bobheck <bobheck11@gmail.com>
2020-10-09 15:25:14 -0500

    interim

---
+++ angmax/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,44 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output('svrCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string }>();
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // contentInput: HTMLInputElement is left in the arguments below 
+  // to show how it would have been used using localref instead
+  // of the @ViewChild method currently in code
+
+  onAddServer(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
+    console.log(this.serverContentInput);
+    // this.serverCreated.emit({
+    //   serverName: nameInput.value, 
+    //   serverContent: contentInput.value
+    // });
+    this.serverCreated.emit({
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+
+}
--- angmax/course-proj/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angmax/course-proj/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1','This is simply a test', 'https://cookieandkate.com/images/2012/03/vegan-curry-recipe.jpg'),
+    new Recipe('Recipe 2','This is simply a test', 'https://cookieandkate.com/images/2012/03/vegan-curry-recipe.jpg'),
+    new Recipe('Recipe 3','This is simply a test', 'https://cookieandkate.com/images/2012/03/vegan-curry-recipe.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
