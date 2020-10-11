56a54e2eb742e531ec98f65b1f6b2dfc5353846a
bobheck <bobheck11@gmail.com>
2020-10-11 15:35:07 -0500

    interim

--- angmax/course-proj/src/app/app.component.ts
+++ angmax/course-proj/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  selectedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.selectedFeature = feature;
+  }
+
 }
