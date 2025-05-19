a033893289ef8e830d69a4c3f283c698903be5ed
Robert Heck <rmheck@sabreindustries.com>
2025-05-19 00:59:59 -0500

    first way of setting class conditionally

--- vuejs-3-b2a/project1/scripts.js
+++ vuejs-3-b2a/project1/scripts.js
@@ -62,13 +62,7 @@ var products = [
     active: false,
     quantity: 1,
   },
-   {
-    photo: "img/water.png",
-    name: "Water Large",
-    price: 2.79,
-    active: false,
-    quantity: 1,
-  },
+  
 ];
 
 const SelfServiceMachine = {
