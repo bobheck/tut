eaac42c445325465cd023cf72607e1ba7c0f8022
Robert Heck <rmheck@sabreindustries.com>
2025-05-20 23:08:52 -0500

    20250520 23:08:52 v-if in show-hide form

--- vuejs-fund-with-comp-api/vid3/src/App.vue
+++ vuejs-fund-with-comp-api/vid3/src/App.vue
@@ -5,6 +5,7 @@ import { ref } from "vue";
 //test  ddfdsaf
 
 const header = ref("Shopping List App");
+const editing = ref(false);
 const items = ref([
   { id: 1, label: "10 party hats" },
   { id: 2, label: "2 board games" },
@@ -13,28 +14,31 @@ const items = ref([
 const newItem = ref("");
 const newItemHighPriority = ref(false);
 const iceCreamFlavors = ref([]);
-const saveItem = ()=>{
-  items.value.push({ id: items.value.length + 1, label: newItem.value })
-  newItem.value = ""
+const saveItem = () => {
+  items.value.push({ id: items.value.length + 1, label: newItem.value });
+  newItem.value = "";
+};
+
+const doEdit = (e)=>{
+  editing.value = e;
+  newItem.value = "";
 }
+
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
-  <form
-    class="add-item-form"
-    @submit.prevent="saveItem"
-  >
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" class="btn btn-cancel" @click="doEdit(false)">Cancel</button>
+    <button v-else class="btn btn-primary" @click="doEdit(true)">Add Item</button>
+  </div>
+  <form class="add-item-form" v-if="editing" @submit.prevent="saveItem">
     <!-- v-model modifiers 
       trim
       lazy
       number 
   -->
-    <input
-      v-model.trim="newItem"
-      type="text"
-      placeholder="Add an item"
-    />
+    <input v-model.trim="newItem" type="text" placeholder="Add an item" />
     <label>
       <input type="checkbox" v-model="newItemHighPriority" />
       High Priority
@@ -46,7 +50,5 @@ const saveItem = ()=>{
       {{ label }}
     </li>
   </ul>
-  <p v-if="!items.length">
-     Nothing to see here
-  </p>
+  <p v-if="!items.length">Nothing to see here</p>
 </template>
