deab8b64fcaa8f717b7f5f6f13bd9885912fc03b
Robert Heck <rmheck@sabreindustries.com>
2025-05-20 20:01:27 -0500

    vid3-stage2-checkbox

--- vuejs-fund-with-comp-api/vid3/src/App.vue
+++ vuejs-fund-with-comp-api/vid3/src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const header = ref('Shopping List App')
+const header = ref("Shopping List App");
 const items = ref([
-  {id: 1, label: "10 party hats"},
-  {id: 2, label:"2 board games"},
-  {id: 3, label: "20 cups"}
-])
-const newItem = ref("")
-const newItemPriority = ref("low")
+  { id: 1, label: "10 party hats" },
+  { id: 2, label: "2 board games" },
+  { id: 3, label: "20 cups" },
+]);
+const newItem = ref("");
+const newItemHighPriority = ref(false);
 //const iceCreamFlavors = ref([])
 </script>
 
@@ -19,19 +19,14 @@ const newItemPriority = ref("low")
       lazy
       number 
   -->
-  <input v-model.trim="newItem" type="text" placeholder="Add an item"> 
-  <label>
-    Priority
-  </label>
-    <select v-model="newItemPriority">
-      <option value="low">Low</option>
-      <option value="high">High</option>
-    </select>
-    <br />
-  {{ newItemPriority }}
+  <input v-model.trim="newItem" type="text" placeholder="Add an item" />
+  <label> High Priority </label>
+  <input type="checkbox" v-model="newItemHighPriority" /> 
+  <br />
+  {{ newItemHighPriority }}
   <ul>
-    <li v-for="({id, label}, index) in items" :key="id">
-      {{label}}
+    <li v-for="({ id, label }, index) in items" :key="id">
+      {{ label }}
     </li>
   </ul>
 </template>
